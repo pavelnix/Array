6eeada73567d3cde1f9a91648fa126b6ee8da2ea
Pavel <pavelnix@yandex.ru>
2015-11-24 12:20:56 -0800

    maven 2

--- test/java/ru/pavelnix/OperationArrayTest.java
+++ test/java/ru/pavelnix/OperationArrayTest.java
@@ -4,6 +4,8 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * Created by Administrator on 24.11.2015.
  */
@@ -19,7 +21,8 @@ public class OperationArrayTest extends TestCase {
 
     @Test
     public void testGetArray() {
-//        operationArray = new OperationArray(array);
-        assertEquals(array, operationArray.getArray());
+        operationArray = new OperationArray(array);
+        int[] array1 = {1, 3};
+        assertArrayEquals(array, operationArray.getArray());
     }
 }
